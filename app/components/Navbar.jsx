64117838d0fd7ce0b016a662541d8ddd143ba76d
mlkiung <meredith.kiung@gmail.com>
2017-05-19 12:04:10 -0400

    eliminated border around navbar custom banner

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,21 +6,21 @@ import AboutMe from 'APP/app/components/AboutMe'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-default color-stormy no-margin">
+    <nav className="nav-giant color-stormy no-margin banner">
       <div className="container-fluid">
-        <div className="navbar-header">
-          <Link className="navbar-brand font-cloudy" to={'/'}>Bear Gets Digital</Link>
+        <div className="navbar-header font-cloudy font-mono">
+          <Link className="navbar-brand" to={'/'}>Bear Gets Digital</Link>
         </div>
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav font-cloudy">
+          <ul className="nav navbar-nav font-cloudy font-sans">
             <li><Link to={'/projects'}>Projects <span className="sr-only">(current)</span></Link></li>
             <li><Link to={'/about-me'}>About Me</Link></li>
           </ul>
           <form className="navbar-form navbar-right" role="search">
             <div className="form-group">
-              <input type="text" className="form-control font-cloudy" placeholder="Search" />
+              <input type="text" className="form-control font-cloudy font-sans" placeholder="Search" />
             </div>
-            <button type="submit" className="btn btn-default btn-submit font-cloudy">Submit</button>
+            <button type="submit" className="btn btn-default btn-submit font-cloudy font-sans">Submit</button>
           </form>
         </div>
       </div>
@@ -29,3 +29,23 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+//    <nav className="navbar navbar-default color-stormy no-margin banner">
+//      <div className="container-fluid">
+//        <div className="navbar-header font-cloudy font-mono">
+//          <Link className="navbar-brand" to={'/'}>Bear Gets Digital</Link>
+//        </div>
+//        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+//          <ul className="nav navbar-nav font-cloudy font-sans">
+//            <li><Link to={'/projects'}>Projects <span className="sr-only">(current)</span></Link></li>
+//            <li><Link to={'/about-me'}>About Me</Link></li>
+//          </ul>
+//          <form className="navbar-form navbar-right" role="search">
+//            <div className="form-group">
+//              <input type="text" className="form-control font-cloudy font-sans" placeholder="Search" />
+//            </div>
+//            <button type="submit" className="btn btn-default btn-submit font-cloudy font-sans">Submit</button>
+//          </form>
+//        </div>
+//      </div>
+//    </nav>
