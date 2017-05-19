49cd2462d7a7bd0825d869d3d7e07fb06cb4c107
mlkiung <meredith.kiung@gmail.com>
2017-05-19 13:25:44 -0400

    fixed styling with navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,21 +6,21 @@ import AboutMe from 'APP/app/components/AboutMe'
 
 const Navbar = () => {
   return (
-    <nav className="nav-giant color-stormy no-margin banner">
-      <div className="container-fluid">
+    <nav className="navbar-default nav-giant color-stormy no-margin banner">
+      <div className="nav-fluid container-fluid">
         <div className="navbar-header font-cloudy font-mono">
-          <Link className="navbar-brand" to={'/'}>Bear Gets Digital</Link>
+          <Link className="nav-navbar-brand navbar-brand" id="nav-brand" to={'/'}>Bear Gets Digital</Link>
         </div>
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav font-cloudy font-sans">
-            <li><Link to={'/projects'}>Projects <span className="sr-only">(current)</span></Link></li>
-            <li><Link to={'/about-me'}>About Me</Link></li>
+          <ul className="nav navbar-nav nav-navbar-nav font-cloudy font-sans">
+            <li><Link id="nav-links" to={'/projects'}>Projects <span className="sr-only">(current)</span></Link></li>
+            <li><Link id="nav-links" to={'/about-me'}>About Me</Link></li>
           </ul>
           <form className="navbar-form navbar-right" role="search">
             <div className="form-group">
-              <input type="text" className="form-control font-cloudy font-sans" placeholder="Search" />
+              <input type="text" className="nav-form-control font-cloudy font-sans" placeholder="Search" />
             </div>
-            <button type="submit" className="btn btn-default btn-submit font-cloudy font-sans">Submit</button>
+            <button type="submit" className="nav-btn nav-btn-default btn-submit font-cloudy font-sans">Submit</button>
           </form>
         </div>
       </div>
