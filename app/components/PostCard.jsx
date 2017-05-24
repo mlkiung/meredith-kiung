d18e707d5139e8c23116215aab5837e766ee4c16
mlkiung <meredith.kiung@gmail.com>
2017-05-24 13:47:18 -0400

    cleaned up unnecessary code

--- app/components/PostCard.jsx
+++ app/components/PostCard.jsx
@@ -21,7 +21,7 @@ class PostCard extends Component {
           <CardMedia>
             <img
               className="img-postcard"
-              src={'http://placehold.it/350x150'}
+              src={'https://placehold.it/350x150'}
               />
           </CardMedia>
           <CardTitle
@@ -39,60 +39,3 @@ class PostCard extends Component {
 }
 
 export default PostCard
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const PostCard = props => {
-
-//   let tags = props.post.tags.split(', ')
-
-//   return (
-//     <div id="PostCard">
-//       <div className="panel panel-default">
-//         <div className="panel-heading" id="post-card-no-border">
-//           <h3 className="panel-title font-serif">{props.post.title}</h3>
-//           <ol className="breadcrumb font-mono">{
-//               tags.map(tag => {
-//                 return (
-//                   <li className="tags" key={tag}>
-//                     <Link to={'/posts'} id="tags-color">{tag}</Link>
-//                   </li>
-//                 )
-//               })
-//             }
-//           </ol>
-//         </div>
-//         <div className="panel-body font-sans">
-//           {props.post.content}
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default PostCard
