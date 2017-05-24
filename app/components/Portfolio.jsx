d621edb27eb578f744799341db30f04e2b74a86c
mlkiung <meredith.kiung@gmail.com>
2017-05-24 12:28:54 -0400

    cleaned up Portfolio.jsx

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -16,11 +16,8 @@ class Portfolio extends Component {
       projects: [],
     }
 
-    // this.makeProjectsArray(this.props.projects)
-
     this.rightOrLeft = this.rightOrLeft.bind(this)
     this.makeProjectsArray = this.makeProjectsArray.bind(this)
-    // this.renderProject = this.renderProject.bind(this)
   }
 
   componentWillReceiveProps(nextProps) {
@@ -49,29 +46,11 @@ class Portfolio extends Component {
     return projectsArr
   }
 
-  // renderProject = (projects) => {
-  //   projects && projects.map((project) => {
-  //     // return this.rightOrLeft(project)
-  //     return <PortfolioCardRight project={project} />
-  //   })
-  // }
-
   render() {
-    const projects = this.state.projects
-
     return (
       <div className="portfolio-container">
-
-        {/*projects && projects.map((project) => {
-          console.log('PROJECT', project)
-          // return this.rightOrLeft(project)
-          return <PortfolioCardRight project={project} key={project.key} />
-        })*/}
-
         {this.state.projects}
-
       </div>
-
     )
   }
 }
@@ -81,38 +60,3 @@ const mapStateToProps = state => ({projects: state.projects})
 const mapDispatchToProps = dispatch => ({ loadAllProjects })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Portfolio)
-
-
-// <div className="outer">
-//           <div className="inner scroll-img" id="cup-of-sugar">CUP OF SUGAR</div>
-//           <div className="inner static-color">
-//             <div className="one-third"></div>
-//             <div className="one-third middle middle-header">
-//               <div className="font-middle-header">CUP OF SUGAR<hr className="hr" /></div>
-//               <div className="font-middle-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-//             </div>
-//             <div className="one-third"></div>
-//           </div>
-//         </div>
-//         <div className="outer">
-//           <div className="inner static-color">
-//             <div className="one-third"></div>
-//             <div className="one-third middle middle-header">
-//               <div className="font-middle-header">CAT MOON DADDY<hr className="hr" /></div>
-//               <div className="font-middle-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-//             </div>
-//             <div className="one-third"></div>
-//           </div>
-//           <div className="inner scroll-img" id="ifoodie">Cat Moon Daddy</div>
-//         </div>
-//         <div className="outer">
-//           <div className="inner scroll-img" id="sandcastle">Firebase Video</div>
-//           <div className="inner static-color">
-//             <div className="one-third"></div>
-//             <div className="one-third middle middle-header">
-//               <div className="font-middle-header">FIREBASE<hr className="hr" /></div>
-//               <div className="font-middle-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-//             </div>
-//             <div className="one-third"></div>
-//           </div>
-//         </div>
