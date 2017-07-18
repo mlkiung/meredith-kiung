55120eb2d45d9e3a054d2205b17d09e50313e07e
mlkiung <meredith.kiung@gmail.com>
2017-07-17 20:41:55 -0400

    updated about me

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -19,11 +19,11 @@ const AboutMe = () => (
       <div className="font-body">*/}
         <div>
           <h1>Hello!</h1>
-          <div>I’m Meredith, a New York City-based Software Engineer with a love of Javascript, puppies, and creating beautiful and responsive web and mobile apps using the latest technologies.</div>
+          <div>I am a New York City-based Software Engineer with a love of Javascript, dogs, and creating beautiful and responsive web and mobile apps.</div>
           <h1>Background</h1>
-          <div>After graduating from Johns Hopkins University with a degree in Psychological & Brain Sciences and a minor in Entrepreneurship & Management, I turned my efforts to helping small businesses stay current in their market. Using a multi-pronged strategic approach that I customized for every business, I maintain a track record of success in taking businesses to the best-in-brand level.</div>
+          <div>I graduated from Johns Hopkins University with a degree in Psychological & Brain Sciences and a minor in Entrepreneurship & Management. Upon graduation, I focused on helping small businesses grow and flourish through disciplined adherence to strategic processes. It proved exhilarating for many years. But I still sought more.</div>
           <h1>Developer Story</h1>
-          <div>I began programming as an undergraduate at Johns Hopkins, and fell in love with it again as an adult. It was the proverbial ‘itch that couldn’t be scratched,’ so I did what any sane, fully-grown human being would do: I dropped everything that had ever made me successful and entered the tech world--not only as a noob, but also as a woman. I enrolled in the only coding bootcamp I wanted to go to--The Grace Hopper Program, which is an all-female division of Fullstack Academy that seeks to close the gender gap and make tech accessible for women. Coincidentally, it ended up being the best experience of my life.</div>
+          <div>I realized that building a technology-based skill set would be the best way to rapidly optimize my value to companies, clients, and organizations. As such, I enrolled in The Grace Hopper Program--the all-female division of Fullstack Academy. I had begun programming as an undergraduate in college. I found myself falling in love with it as a young professional.</div>
           <h1>Skills</h1>
           <div>
             <ul>
@@ -58,15 +58,12 @@ const AboutMe = () => (
           </div>
           <h1>Contact Me</h1>
           <div>
-            I love hearing from people!
+            I absolutely love to hear from people.
             <br/>
+            Be sure to check out my blog (link above), where I post everything from tutorials and tips that I’ve found useful, to awkward stories and my thoughts on being a young female developer living and working in New York City.
             <br/>
-            Be sure to check out my blog (link above!), where I post everything from tutorials and tips that I’ve found useful, to awkward stories and my thoughts on being a young female developer living and working in New York City.
-            <br/>
-            <br/>
-            Also head over to my photo blog on Instagram, @code.ess, where I visually document my trials and tribulations--and sometimes the occasional coding meme :)
+            Also head over to my photo blog on Instagram, @code.ess, where I visually document my trials and tribulations--and sometimes the occasional coding meme.
           </div>
-        {/*</div>*/}
       </div>
       <div id="contact-icons">
         <a href="https://github.com/mlkiung" target="_blank"><span className="fa fa-github fa-2x about-icons" aria-hidden="true"></span></a>
